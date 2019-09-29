d2bd7db8c8d0293720cd79047b939a27e258d406
nolleh <nolleh7707@gmail.com>
2019-09-29 19:19:52 +0900

    transition

--- 04.week4/src/transition/App.vue
+++ 04.week4/src/transition/App.vue
@@ -1,9 +1,31 @@
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.14.1/lodash.min.js"></script>
 <template>
-  <div id="transition">
-    <button @click="show = !show">Toggle</button>
-    <transition name="fade">
-      <p v-if="show">hello</p>
-    </transition>
+  <div>
+    <div id="transition">
+      <button @click="show = !show">Toggle</button>
+      <transition name="fade">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>
+
+    <div id="example-2">
+      <button @click="show = !show">Toggle show</button>
+      <transition name="bounce">
+        <p
+          v-if="show"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis enim libero, at lacinia diam fermentum id. Pellentesque habitant morbi tristique senectus et netus.</p>
+      </transition>
+    </div>
+
+    <div id="list-complete-demo" class="demo">
+      <button v-on:click="shuffle">Shuffle</button>
+      <button v-on:click="add">Add</button>
+      <button v-on:click="remove">Remove</button>
+      <transition-group name="list-complete" tag="p">
+        <span v-for="item in items" v-bind:key="item" class="list-complete-item">{{ item }}</span>
+      </transition-group>
+    </div>
   </div>
 </template>
 
@@ -13,7 +35,24 @@ export default Vue.extend({
   data() {
     return {
       show: true,
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      nextNum: 10,
     };
+  },
+
+  methods: {
+    randomIndex: function() {
+      return Math.floor(Math.random() * this.items.length);
+    },
+    add: function() {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++);
+    },
+    remove: function() {
+      this.items.splice(this.randomIndex(), 1);
+    },
+    shuffle: function() {
+      this.items = _.shuffle(this.items);
+    },
   },
 });
 </script>
@@ -26,5 +65,52 @@ export default Vue.extend({
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
+}
+
+.bounce-enter-active {
+  animation: bounce-in1 0.5s;
+}
+
+.bounce-leave-active {
+  animation: bounce-in1 0.5s reverse;
+}
+
+@keyframes bounce-in1 {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.list-complete-item {
+  transition: all 1s;
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-complete-enter, .list-complete-leave-to
+/* .list-complete-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
 }
 </style>
