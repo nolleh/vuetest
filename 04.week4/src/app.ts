00570166b6891a954bdc9d5f55afa720146b4881
nolleh <nolleh7707@gmail.com>
2019-09-29 17:13:31 +0900

    transition

--- 04.week4/src/app.ts
+++ 04.week4/src/app.ts
@@ -1,6 +1,12 @@
 import Vue from 'vue';
-import Slot from './slot/App.vue';
+// import Slot from './slot/App.vue';
+
+// new Vue({
+//   render: h => h(Slot),
+// }).$mount('#app');
+
+import transition from './transition/App.vue';
 
 new Vue({
-  render: h => h(Slot),
+  render: h => h(transition),
 }).$mount('#app');
---
+++ 04.week4/src/transition/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="transition">
+    <button @click="show = !show">Toggle</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue/dist/vue';
+export default Vue.extend({
+  data() {
+    return {
+      show: true,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
